1d1882722ec719cb165ec29c9ebd5d82ea1583ec
marcosti <marcosti096@duck.com>
2023-12-31 16:48:22 +0100

    minor style changes

--- src/lib/brsar.c
+++ src/lib/brsar.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 
 //MAIN FUNCTIONS
-void analyzeFile(const char *filename)
+void file_info(const char *filename)
 {
-    printf("Analyzing file %s...\n", filename);
+    printf("\e[1;32mINFO\e[0m: Analyzing file %s...\n", filename);
     FILE *file;
     file = fopen(filename, "rb");
     if (!file)
     {
-        printf("Error: %s doesn't exist.\n", filename);
+        fprintf(stderr, "\e[1;31mERROR\e[0m: %s doesn't exist.\n", filename);
         exit(EXIT_FAILURE);
     }
 
-    Header header;
+    brsar_header_t header;
     if (_read_header(file, &header))
     {
         if (_is_big_endian(&header))
         {
             _swap_header(&header);
         }
-        _header_to_string(&header);
+        header_contents(&header);
     }
     else
     {
         // Failed to read header
-        printf("Error: Failed to read header\n");
+        fputs("\e[1;31mERROR\e[0m: Failed to read header\n", stderr);
         exit(EXIT_FAILURE);
     }
 
     fclose(file);
 }
 
-void dumpFile(const char *filename)
+void file_dump(const char *filename)
 {
     // Implement the file dump logic
-    printf("Dumping file %s...\n", filename);
+    printf("\e[1;32mINFO\e[0m: Dumping file %s...\n", filename);
 }
 
 
 //CORE FUNCTIONS
 // Swap `header`variables between big endian and little endian notation
-void _swap_header(Header *header)
+void _swap_header(brsar_header_t *header)
 {
     if (!header)
     {
@@ -57,20 +57,20 @@ void _swap_header(Header *header)
     header->fileOffset = _swap32(header->fileOffset);
     header->fileSize = _swap32(header->fileSize);
 }
-// Print Header struct in a human readable format to standard output.
-void _header_to_string(Header *header)
+// Print brsar_header_t struct in a human readable format to standard output.
+void header_contents(brsar_header_t *header)
 {
 
     if (!header)
         return;
     printf("\n////HEADER////\n");
-    printf("magic: %s\n", _is_BRSAR_magic(header) ? "valid" : "invalid");
-    printf("big endian: %s\n", _is_big_endian(header) ? "true" : "false");
+    printf("Magic: %s\n", _is_rsar_magic(header) ? "valid" : "invalid");
+    printf("Endianness: %s\n", _is_big_endian(header) ? "true" : "false");
  
     uint8_t byte1 = (header->version >> 8) & 0xFF;
     uint8_t byte2 = header->version & 0xFF;
-    printf("version: %02X %02X\n", byte1, byte2);
-    printf("file lenght: %u\n", header->fileLength);
+    printf("Version: %02X %02X\n", byte1, byte2);
+    printf("File length: %u\n", header->fileLength);
     printf("Number of sections: %u\n", header->numSections);
     printf("Offset to the SYMB section: %u\n", header->symbOffset);
     printf("Size of the SYMB section: %u\n", header->symbSize);
@@ -80,14 +80,14 @@ void _header_to_string(Header *header)
     printf("Size of the FILE section %u\n", header->fileSize);
 }
 // Read the first 40 bytes of `file` and update `header`struct. Return 1 if successful.
-int _read_header(FILE *file, Header *header)
+bool _read_header(FILE *file, brsar_header_t *header)
 {
-    if (file == NULL || header == NULL)
+    if (!file || !header)
     {
         return 0;
     }
     // Read the header from the file
-    size_t bytesRead = fread(header, sizeof(Header), 1, file);
+    size_t bytesRead = fread(header, sizeof(brsar_header_t), 1, file);
 
     // Check if the correct number of bytes were read
     if (bytesRead != 1)
@@ -101,23 +101,21 @@ int _read_header(FILE *file, Header *header)
 }
 
 //UTILS FUNCTIONS
-bool _is_BRSAR_magic(Header *header)
-{
+void _null_header(brsar_header_t *header) {
     if (!header)
     {
-        printf("Error: header is NULL\n");
+        fputs("\e[1;31merror\e[0m: header is null\n", stderr);
         exit(EXIT_FAILURE);
     }
-    return (header->magic[0] == 'R' && header->magic[1] == 'S' && header->magic[2] == 'A' && header->magic[3] == 'R') ||
-           (header->magic[3] == 'R' && header->magic[2] == 'S' && header->magic[1] == 'A' && header->magic[0] == 'R');
 }
-bool _is_big_endian(Header *header)
+bool _is_rsar_magic(brsar_header_t *header)
 {
-    if (!header)
-    {
-        printf("Error: header is NULL\n");
-        exit(EXIT_FAILURE);
-    }
+    _null_header(header);
+    return (!strncmp(header->magic, "RSAR", 4));
+}
+bool _is_big_endian(brsar_header_t *header)
+{
+    _null_header(header);
     return header->bom == 0xFFFE;
 }
 uint16_t _swap16(uint16_t n)
